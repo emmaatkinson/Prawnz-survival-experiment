c2be86c06c289746185b626fc1289297fd61b8f8
jacobhoutman <jacobhoutman123@gmail.com>
2023-06-02 15:33:30 -0700

    gumbo

--- analysis/2023_05_31_model_fitting.R
+++ analysis/2023_05_31_model_fitting.R
@@ -16,9 +16,11 @@ trial<-trial[order(trial$trial_number),]
 model_df<-survival[(is.na(survival$treatment)==FALSE),]
 model_df$treatment<-as.integer(model_df$treatment)
 model_df<-model_df[order(model_df$trial_number,model_df$prawn_id),]
-model_df$trial_trap<-6*(model_df$trial_number-1)+model_df$trap_number
-model_df[which(model_df$trial_number>11),]$trial_trap<-model_df[which(model_df$trial_number>11),]$trial_trap-12
+model_df$trial_trap<-paste(model_df$trial_number,"-",model_df$trap_number)
+unique(model_df$trial_trap)
+
 model_df_1<-model_df[c(-564,-1255),]
+unique(model_df_1$trial_trap)
 model_df_1$temp<-rep(0, nrow(model_df_1))
 n_trials<-nrow(trial)
 temp_dummy<-(trial$exp_set_temp_air-mean(trial$exp_set_temp_air))/sqrt(var(trial$exp_set_temp_air))
@@ -35,21 +37,20 @@ model_df_1$trial_trap<-as.factor(model_df_1$trial_trap)
 
 
 model_df_1$length<-round(model_df_1$length/0.5)*0.5
-
 model_df_1$length<-(model_df_1$length-mean(model_df_1$length, na.rm=TRUE))/sqrt(var(model_df_1$length, na.rm = TRUE))
-model_df_1$temp<-(model_df_1$temp-mean(model_df_1$temp))/sqrt(var(model_df_1$temp))
 
-model_df_1
+
+mean(model_df_1$length, na.rm=TRUE)
 install.packages("glmmTMB")
 install.packages("lme4")
-names(model_df_1)
-sum(is.na(model_df_1))-sum(is.na(model_df_1$comments))-sum(is.na(model_df_1$length))-sum(is.na(model_df_1$stage))
 
-
-
+library("glmmTMB")
+library("lme4")
+summary(model_ttl1)
+summary(model_big2)
 #Intercept only model
-model_null1<-glmer(alive~(1|trial_trap),data=model_df_1,family=binomial)
-model_null2<-glmmTMB(alive~(1|trial_trap),data=model_df_1,family=binomial)
+model_null1<-lme4::glmer(alive~(1|trial_trap),data=model_df_1,family=binomial)
+model_null2<-glmmTMB::glmmTMB(alive~(1|trial_trap),data=model_df_1,family=binomial)
 
 #treatment model
 model_treat1<-glmer(alive~treatment+(1|trial_trap),data=model_df_1,family=binomial)
@@ -81,6 +82,51 @@ model_big2<-glmmTMB(alive~treatment+temp+length+temp*length+treatment*length+(1|
 
 
 
+
+install.packages("MuMIn") # needed for BIC weights
+
+# Get BIC values for each mixed-effects model
+BIC.values <- BIC(model_null2, model_treat2, model_temp2, model_length2, model_tt2,model_tti2, model_ttl2,model_big2)
+
+# Shorthand names for models (in the same order as in BIC.values
+BIC.names <- c("Null", "Treatment", "Temperature", "Length","Treatment + Temperature", "Treatment + Temperature + Treatment x Temperature","Treatment + Temperature + Length","Treatment + Temperature + Length + Treatment x Length + Temperature x Length")
+
+# Order the BIC and model name vectors by ascending order of the AIC vector elements
+BIC.order <- order(BIC.values$BIC)
+BIC.names <- BIC.names[BIC.order]
+BIC.values <- BIC.values$BIC[BIC.order]
+
+# Calculate BIC weights 
+BIC.weights <- MuMIn::Weights(BIC.values)
+
+# Calculate BIC cumulative weights
+BIC.cumul.weights <- numeric(length(BIC.weights))
+BIC.cumul.weights[1] <- BIC.weights[1]
+for (i in 2:length(BIC.weights)) {
+  BIC.cumul.weights[i] <- BIC.cumul.weights[i-1] + BIC.weights[i]
+}
+
+# Calculate maximum log likelihood values and re-order
+BIC.logLik <- c(logLik(model_null2), logLik(model_length2), logLik(model_temp2), logLik(model_treat2), logLik(model_tt2),logLik(model_tti2),logLik(model_ttl2),logLik(model_big2))
+BIC.logLik <- BIC.logLik[BIC.order]
+
+
+# Create delta BIC function and calculate for each model relative to top model
+deltaFcn = function(listBIC) {
+  deltas = -(min(listBIC) - listBIC)
+}
+BIC.deltas = deltaFcn(BIC.values)
+
+# Create BIC table
+BIC.table <- data.frame(
+  model.name = BIC.names, BIC=BIC.values,
+  deltaBIC = round(BIC.deltas,2),
+  weight = round(BIC.weights,3),
+  cumul.weight = round(BIC.cumul.weights,3),
+  logLik = round(BIC.logLik,2))
+# Save BIC table #
+setwd(here("figures"))
+write.csv(BIC.table, paste(Sys.Date(),"Prawn_Survival_BIC_table.csv"))
 
 
 
--- data-clean/2023-06-01_reflex_analysis.R
+++ data-clean/2023-06-01_reflex_analysis.R
@@ -1,5 +1,6 @@
 setwd("/Users/jacobhoutman/Documents/Git Hub/Prawnz-survival-experiment")
-setwd(here("data-raw"))
+library("here")
+setwd(here("data-clean"))
 reflexes<-read.csv("2023-05-09_prawn_combined_reflex_data.csv")
 survival<-read.csv("2023-05-09_prawn_combined_survival_data.csv")
 trial<-read.csv("2023-05-09_prawn_combined_trial_data.csv")
@@ -7,6 +8,39 @@ trial<-read.csv("2023-05-09_prawn_combined_trial_data.csv")
 reflexes<-reflexes[order(reflexes$trial_number,reflexes$trap_number),]
 names(reflexes)
 reflexes[which(is.na(reflexes$abdomen_turgor+reflexes$abdomen_retraction+reflexes$leg_movement+reflexes$leg_retraction+reflexes$maxilliped_movement+reflexes$maxilliped_retraction+reflexes$antenna+reflexes$eye_turgor+as.integer(reflexes$pleopods)+reflexes$mouth)),]
+
+reflexes<-reflexes[order(reflexes$trial_number,reflexes$trap_number),]
+reflexes$abdomen_turgor+reflexes$abdomen_retraction+reflexes$leg_movement+reflexes$leg_retraction+reflexes$maxilliped_movement+reflexes$maxilliped_retraction+reflexes$antenna+reflexes$eye_turgor+as.integer(reflexes$pleopods)+reflexes$mouth
+reflexes$total<-reflexes$abdomen_turgor+reflexes$abdomen_retraction+reflexes$leg_movement+reflexes$leg_retraction+reflexes$maxilliped_movement+reflexes$maxilliped_retraction+reflexes$antenna+reflexes$eye_turgor+as.integer(reflexes$pleopods)+reflexes$mouth
+setwd(here("figures"))
+violin_reflex<-data.frame(total=c(reflexes[which(reflexes$treatment==0),]$total,reflexes[which(reflexes$treatment==30),]$total,reflexes[which(reflexes$treatment==60),]$total,reflexes[which(reflexes$treatment==90),]$total,reflexes[which(reflexes$treatment==120),]$total),treat=c(rep("0",nrow(reflexes[which(reflexes$treatment==0),])),rep("30",nrow(reflexes[which(reflexes$treatment==30),])),rep("60",nrow(reflexes[which(reflexes$treatment==60),])),rep("90",nrow(reflexes[which(reflexes$treatment==90),])),rep("120",nrow(reflexes[which(reflexes$treatment==120),]))))
+violin_reflex$treat<-as.factor(violin_reflex$treat)
+ggplot(violin_reflex, aes(x=treat, y=total)) + geom_violin()
+
+nrow(reflexes[which(reflexes$treatment==120),])
+
+boxplot(reflexes[which(reflexes$treatment==0),]$total,reflexes[which(reflexes$treatment==30),]$total,reflexes[which(reflexes$treatment==60),]$total,reflexes[which(reflexes$treatment==90),]$total,reflexes[which(reflexes$treatment==120),]$total)
+graphics.off()
+
+setwd(here("figures"))
+pdf(paste(Sys.Date(), "reflex_distribution.pdf", sep="_"), width=7, height=7, pointsize=12)
+par(mfrow=c(6,2),mar=c(4,4,1,2), oma=c(0,0,4,0))
+for (i in 0:4){
+  treat_ref<-reflexes[which(reflexes$treatment==i*30),]$total
+  plot(NULL, xlim=c(0,10),ylim=c(0,100), main=paste("Reflex distribution trial", i*30))
+  points(0,sum(treat_ref==0)*100/length(treat_ref==0))
+  points(1,sum(treat_ref==1)*100/length(treat_ref==1))
+  points(2,sum(treat_ref==2)*100/length(treat_ref==2))
+  points(3,sum(treat_ref==3)*100/length(treat_ref==3))
+  points(4,sum(treat_ref==4)*100/length(treat_ref==4))
+  points(5,sum(treat_ref==5)*100/length(treat_ref==5))
+  points(6,sum(treat_ref==6)*100/length(treat_ref==6))
+  points(7,sum(treat_ref==7)*100/length(treat_ref==7))
+  points(8,sum(treat_ref==8)*100/length(treat_ref==8))
+  points(9,sum(treat_ref==9)*100/length(treat_ref==9))
+  points(10,sum(treat_ref==10)*100/length(treat_ref==10))
+}
+dev.off()
 
 
 
