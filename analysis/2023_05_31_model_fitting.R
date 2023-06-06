c83f60e19dd59e914905a1b67e11724309461935
jacobhoutman <jacobhoutman123@gmail.com>
2023-06-05 17:04:09 -0700

    june 5

--- analysis/2023_05_31_model_fitting.R
+++ analysis/2023_05_31_model_fitting.R
@@ -48,37 +48,174 @@ library("glmmTMB")
 library("lme4")
 summary(model_ttl1)
 summary(model_big2)
+model_df_1
+model_df_2<-model_df_1[is.na(model_df_1$length)==FALSE,]
 #Intercept only model
-model_null1<-lme4::glmer(alive~(1|trial_trap),data=model_df_1,family=binomial)
-model_null2<-glmmTMB::glmmTMB(alive~(1|trial_trap),data=model_df_1,family=binomial)
+model_null1<-lme4::glmer(alive~(1|trial_trap),data=model_df_2,family=binomial)
+model_null2<-glmmTMB::glmmTMB(alive~(1|trial_trap),data=model_df_2,family=binomial)
 
 #treatment model
-model_treat1<-glmer(alive~treatment+(1|trial_trap),data=model_df_1,family=binomial)
-model_treat2<-glmmTMB(alive~treatment+(1|trial_trap),data=model_df_1,family=binomial)
+model_treat1<-glmer(alive~treatment+(1|trial_trap),data=model_df_2,family=binomial)
+model_treat2<-glmmTMB(alive~treatment+(1|trial_trap),data=model_df_2,family=binomial)
 
 #temp model
-model_temp1<-glmer(alive~temp+(1|trial_trap),data=model_df_1,family=binomial)
-model_temp2<-glmmTMB(alive~temp+(1|trial_trap),data=model_df_1,family=binomial)
+model_temp1<-glmer(alive~temp+(1|trial_trap),data=model_df_2,family=binomial)
+model_temp2<-glmmTMB(alive~temp+(1|trial_trap),data=model_df_2,family=binomial)
 
 #length model
-model_length1<-glmer(alive~length+(1|trial_trap),data=model_df_1,family=binomial)
-model_length2<-glmmTMB(alive~length+(1|trial_trap),data=model_df_1,family=binomial)
+model_length1<-glmer(alive~length+(1|trial_trap),data=model_df_2,family=binomial)
+model_length2<-glmmTMB(alive~length+(1|trial_trap),data=model_df_2,family=binomial)
 
 #treatment and temp model 
-model_tt1<-glmer(alive~treatment+temp+(1|trial_trap),data=model_df_1,family=binomial)
-model_tt2<-glmmTMB(alive~treatment+temp+(1|trial_trap),data=model_df_1,family=binomial)
+model_tt1<-glmer(alive~treatment+temp+(1|trial_trap),data=model_df_2,family=binomial)
+model_tt2<-glmmTMB(alive~treatment+temp+(1|trial_trap),data=model_df_2,family=binomial)
+
+#temp and length model 
+model_templ1<-glmer(alive~length+temp+(1|trial_trap),data=model_df_2,family=binomial)
+model_templ2<-glmmTMB(alive~length+temp+(1|trial_trap),data=model_df_2,family=binomial)
+
+#treatment and length model 
+model_treatl1<-glmer(alive~treatment+length+(1|trial_trap),data=model_df_2,family=binomial)
+model_treatl2<-glmmTMB(alive~treatment+length+(1|trial_trap),data=model_df_2,family=binomial)
 
 #treatment and temp and interaction model 
-model_tti1<-glmer(alive~treatment+temp+treatment*temp+(1|trial_trap),data=model_df_1,family=binomial)
-model_tti2<-glmmTMB(alive~treatment+temp+treatment*temp+(1|trial_trap),data=model_df_1,family=binomial)
+model_tti1<-glmer(alive~treatment+temp+treatment*temp+(1|trial_trap),data=model_df_2,family=binomial)
+model_tti2<-glmmTMB(alive~treatment+temp+treatment*temp+(1|trial_trap),data=model_df_2,family=binomial)
 
 #treatment and temp and length model 
-model_ttl1<-glmer(alive~treatment+temp+length+(1|trial_trap),data=model_df_1,family=binomial)
-model_ttl2<-glmmTMB(alive~treatment+temp+length+(1|trial_trap),data=model_df_1,family=binomial)
+model_ttl1<-glmer(alive~treatment+temp+length+(1|trial_trap),data=model_df_2,family=binomial)
+model_ttl2<-glmmTMB(alive~treatment+temp+length+(1|trial_trap),data=model_df_2,family=binomial)
 
 #treatment and temp and length and length interactions model
-model_big1<-glmer(alive~treatment+temp+length+temp*length+treatment*length+(1|trial_trap),data=model_df_1,family=binomial)
-model_big2<-glmmTMB(alive~treatment+temp+length+temp*length+treatment*length+(1|trial_trap),data=model_df_1,family=binomial)
+model_big1<-glmer(alive~treatment+temp+length+temp*length+treatment*length+(1|trial_trap),data=model_df_2,family=binomial)
+model_big2<-glmmTMB(alive~treatment+temp+length+temp*length+treatment*length+(1|trial_trap),data=model_df_2,family=binomial)
+
+
+
+#ANALYSIS AND PLOTTING
+
+back_trans<-function(x){
+  return(exp(x)/(1+exp(x)))
+}
+trans<-function(x){
+  return(log(x/(1-x)))
+}
+back_trans_1<-function(x){
+  return(exp(0.3-0.14*x)/(1+exp(0.3-0.14*x)))
+}
+
+
+plot(seq(-100,100,1),back_trans_1(seq(-100,100,1)))
+
+
+
+pframe <- data.frame(trial_trap=factor(levels(model_df_2$trial_trap), levels=levels(model_df_2$trial_trap)))
+cpred1 <- predict(model_length1,re.form=NA,newdata=pframe,type="response")
+
+easyPredCI <- function(model,newdata=NULL,alpha=0.05) {
+  ## baseline prediction, on the linear predictor (logit) scale:
+  pred0 <- predict(model,re.form=NA,newdata=newdata)
+  ## fixed-effects model matrix for new data
+  X <- model.matrix(formula(model,fixed.only=TRUE)[-2],newdata)
+  beta <- fixef(model) ## fixed-effects coefficients
+  V <- vcov(model)     ## variance-covariance matrix of beta
+  pred.se <- sqrt(diag(X %*% V %*% t(X))) ## std errors of predictions
+  ## inverse-link function
+  linkinv <- family(model)$linkinv
+  ## construct 95% Normal CIs on the link scale and
+  ##  transform back to the response (probability) scale:
+  crit <- -qnorm(alpha/2)
+  linkinv(cbind(conf.low=pred0-crit*pred.se,
+                conf.high=pred0+crit*pred.se))
+}
+cpred1.CI <- easyPredCI(cmod_lme4_L,pframe)
+
+
+
+g_pframe <- cbind(expand.grid(year=2004:2006,prev=0:80),Area=1)
+g_pred <- predict(gmod_lme4_L,newdata=g_pframe,re.form=NA,
+                  type="response")
+g_predCI <- easyPredCI(gmod_lme4_L,newdata=g_pframe)
+set.seed(101)
+g_bb <- bootMer(gmod_lme4_L,FUN=function(x),predict(x,re.form=NA,newdata=g_pframe,type="response"),nsim=400)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+easyPredCI <- function(model,newdata=NULL,alpha=0.05) {
+  ## baseline prediction, on the linear predictor (logit) scale:
+  pred0 <- predict(model,re.form=NA,newdata=newdata)
+  ## fixed-effects model matrix for new data
+  X <- model.matrix(formula(model,fixed.only=TRUE)[-2],model.frame(model))
+  beta <- fixef(model) ## fixed-effects coefficients
+  V <- vcov(model)     ## variance-covariance matrix of beta
+  pred.se <- sqrt(diag(X %*% V %*% t(X))) ## std errors of predictions
+  ## inverse-link function
+  linkinv <- family(model)$linkinv
+  ## construct 95% Normal CIs on the link scale and
+  ##  transform back to the response (probability) scale:
+  crit <- -qnorm(alpha/2)
+  linkinv(cbind(conf.low=pred0-crit*pred.se,conf.high=pred0+crit*pred.se))
+}
+
+
+plot(sort(model_df_2$length),back_trans(summary(model_length1)$coef[1]+summary(model_length1)$coef[2]*sort(model_df_2$length)),ylim=c(0,1))
+lines(model_df_2$length,easyPredCI(model_length1)[,1])
+lines(model_df_2$length,easyPredCI(model_length1)[,2])
+
+
+plot(sort(model_df_2$treatment),back_trans(summary(model_treat1)$coef[1]+summary(model_treat1)$coef[2]*sort(model_df_2$treatment)),ylim=c(0,1))
+points(model_df_2$treatment,easyPredCI(model_treat1)[,1], col="red")
+points(model_df_2$treatment,easyPredCI(model_treat1)[,2], col="blue")
+
+
+plot(sort(model_df_2$treatment),back_trans(summary(model_tti1)$coef[1]+summary(model_tti1)$coef[2]*sort(model_df_2$treatment)),ylim=c(0,1))
+points(model_df_2$treatment,easyPredCI(model_treat1)[,1], col="red")
+points(model_df_2$treatment,easyPredCI(model_treat1)[,2], col="blue")
+
+
+
+
+
+
+plot(model_df_2$treatment, model_df_2$alive)
+points(sort(model_df_2$length),back_trans(summary(model_length1)$coef[1]+summary(model_length1)$coef[2]*sort(model_df_2$length)))
+
+
+
+
+
+
+
+
+
+
+
+BIC.names
+fixef(model_tt2)
+
 
 
 
@@ -86,10 +223,10 @@ model_big2<-glmmTMB(alive~treatment+temp+length+temp*length+treatment*length+(1|
 install.packages("MuMIn") # needed for BIC weights
 
 # Get BIC values for each mixed-effects model
-BIC.values <- BIC(model_null2, model_treat2, model_temp2, model_length2, model_tt2,model_tti2, model_ttl2,model_big2)
+BIC.values <- BIC(model_null2, model_treat2, model_temp2, model_length2, model_tt2,model_templ2,model_treatl2,model_tti2, model_ttl2,model_big2)
 
 # Shorthand names for models (in the same order as in BIC.values
-BIC.names <- c("Null", "Treatment", "Temperature", "Length","Treatment + Temperature", "Treatment + Temperature + Treatment x Temperature","Treatment + Temperature + Length","Treatment + Temperature + Length + Treatment x Length + Temperature x Length")
+BIC.names <- c("Null", "Treatment", "Temperature", "Length","Treatment + Temperature","Temperature + Length","Treatment + Length", "Treatment + Temperature + Treatment x Temperature","Treatment + Temperature + Length","Treatment + Temperature + Length + Treatment x Length + Temperature x Length")
 
 # Order the BIC and model name vectors by ascending order of the AIC vector elements
 BIC.order <- order(BIC.values$BIC)
@@ -107,7 +244,7 @@ for (i in 2:length(BIC.weights)) {
 }
 
 # Calculate maximum log likelihood values and re-order
-BIC.logLik <- c(logLik(model_null2), logLik(model_length2), logLik(model_temp2), logLik(model_treat2), logLik(model_tt2),logLik(model_tti2),logLik(model_ttl2),logLik(model_big2))
+BIC.logLik <- c(logLik(model_null2), logLik(model_treat2), logLik(model_temp2), logLik(model_length2), logLik(model_tt2),logLik(model_templ2),logLik(model_treatl2),logLik(model_tti2),logLik(model_ttl2),logLik(model_big2))
 BIC.logLik <- BIC.logLik[BIC.order]
 
 
@@ -124,16 +261,13 @@ BIC.table <- data.frame(
   weight = round(BIC.weights,3),
   cumul.weight = round(BIC.cumul.weights,3),
   logLik = round(BIC.logLik,2))
-# Save BIC table #
+formula(model_big1)
+
+
+
+# Save BIC table 
 setwd(here("figures"))
 write.csv(BIC.table, paste(Sys.Date(),"Prawn_Survival_BIC_table.csv"))
-
-
-
-
-
-
-
 
 
 
--- data-clean/2023-06-01_reflex_analysis.R
+++ data-clean/2023-06-01_reflex_analysis.R
@@ -5,29 +5,29 @@ reflexes<-read.csv("2023-05-09_prawn_combined_reflex_data.csv")
 survival<-read.csv("2023-05-09_prawn_combined_survival_data.csv")
 trial<-read.csv("2023-05-09_prawn_combined_trial_data.csv")
 
+#Ordering dataframe
 reflexes<-reflexes[order(reflexes$trial_number,reflexes$trap_number),]
-names(reflexes)
-reflexes[which(is.na(reflexes$abdomen_turgor+reflexes$abdomen_retraction+reflexes$leg_movement+reflexes$leg_retraction+reflexes$maxilliped_movement+reflexes$maxilliped_retraction+reflexes$antenna+reflexes$eye_turgor+as.integer(reflexes$pleopods)+reflexes$mouth)),]
 
-reflexes<-reflexes[order(reflexes$trial_number,reflexes$trap_number),]
-reflexes$abdomen_turgor+reflexes$abdomen_retraction+reflexes$leg_movement+reflexes$leg_retraction+reflexes$maxilliped_movement+reflexes$maxilliped_retraction+reflexes$antenna+reflexes$eye_turgor+as.integer(reflexes$pleopods)+reflexes$mouth
+#New column for sum of reflexes 
 reflexes$total<-reflexes$abdomen_turgor+reflexes$abdomen_retraction+reflexes$leg_movement+reflexes$leg_retraction+reflexes$maxilliped_movement+reflexes$maxilliped_retraction+reflexes$antenna+reflexes$eye_turgor+as.integer(reflexes$pleopods)+reflexes$mouth
+
+#Violin plot sum of reflexes for each treatment
 setwd(here("figures"))
 violin_reflex<-data.frame(total=c(reflexes[which(reflexes$treatment==0),]$total,reflexes[which(reflexes$treatment==30),]$total,reflexes[which(reflexes$treatment==60),]$total,reflexes[which(reflexes$treatment==90),]$total,reflexes[which(reflexes$treatment==120),]$total),treat=c(rep("0",nrow(reflexes[which(reflexes$treatment==0),])),rep("30",nrow(reflexes[which(reflexes$treatment==30),])),rep("60",nrow(reflexes[which(reflexes$treatment==60),])),rep("90",nrow(reflexes[which(reflexes$treatment==90),])),rep("120",nrow(reflexes[which(reflexes$treatment==120),]))))
 violin_reflex$treat<-as.factor(violin_reflex$treat)
 ggplot(violin_reflex, aes(x=treat, y=total)) + geom_violin()
 
-nrow(reflexes[which(reflexes$treatment==120),])
 
 boxplot(reflexes[which(reflexes$treatment==0),]$total,reflexes[which(reflexes$treatment==30),]$total,reflexes[which(reflexes$treatment==60),]$total,reflexes[which(reflexes$treatment==90),]$total,reflexes[which(reflexes$treatment==120),]$total)
 graphics.off()
 
+#Plot for each treatment 
 setwd(here("figures"))
 pdf(paste(Sys.Date(), "reflex_distribution.pdf", sep="_"), width=7, height=7, pointsize=12)
 par(mfrow=c(6,2),mar=c(4,4,1,2), oma=c(0,0,4,0))
 for (i in 0:4){
   treat_ref<-reflexes[which(reflexes$treatment==i*30),]$total
-  plot(NULL, xlim=c(0,10),ylim=c(0,100), main=paste("Reflex distribution trial", i*30))
+  plot(NULL, xlim=c(0,10),ylim=c(0,100), main=paste("Reflex distribution treatment", i*30))
   points(0,sum(treat_ref==0)*100/length(treat_ref==0))
   points(1,sum(treat_ref==1)*100/length(treat_ref==1))
   points(2,sum(treat_ref==2)*100/length(treat_ref==2))
@@ -42,8 +42,14 @@ for (i in 0:4){
 }
 dev.off()
 
+quantile(x, seq(0.2,0.8,0.2))
+plot(NULL, xlim=c(0,120),ylim=c(0,10), main="Reflex quantiles by treatment")
+for (i in 0:4){
+  treat_ref<-reflexes[which(reflexes$treatment==i*30),]$total
+  points(rep(i*30, 4), quantile(treat_ref,seq(0.2,0.8,0.2)))
+}
 
-
+ quantile(reflexes[which(reflexes$treatment==90),]$total,seq(0.2,0.8,0.2))
 
 
 
